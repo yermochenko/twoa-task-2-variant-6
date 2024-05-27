149ba4ffa79b999a0c1de43a496ebcf3089c6aee
Sergey Yermochenko <yermochenko@gmail.com>
2024-05-27 15:46:02 +0300

    FR3 и FR4 завершены

--- src/main/java/by/vsu/twoa/domain/Task.java
+++ src/main/java/by/vsu/twoa/domain/Task.java
@@ -1,11 +1,16 @@
 package by.vsu.twoa.domain;
 
+import by.vsu.twoa.geometry.Point;
+import by.vsu.twoa.geometry.Triangle;
+
 import java.util.Date;
 
 public class Task extends Entity {
 	private User owner;
 	private String name;
 	private Date created;
+	private Triangle triangle;
+	private Point altitudeIntersectionPoint;
 
 	public User getOwner() {
 		return owner;
@@ -29,5 +34,21 @@ public class Task extends Entity {
 
 	public void setCreated(Date created) {
 		this.created = created;
+	}
+
+	public Triangle getTriangle() {
+		return triangle;
+	}
+
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+	public Point getAltitudeIntersectionPoint() {
+		return altitudeIntersectionPoint;
+	}
+
+	public void setAltitudeIntersectionPoint(Point altitudeIntersectionPoint) {
+		this.altitudeIntersectionPoint = altitudeIntersectionPoint;
 	}
 }
--- src/main/java/by/vsu/twoa/service/TaskService.java
+++ src/main/java/by/vsu/twoa/service/TaskService.java
@@ -5,9 +5,15 @@ import by.vsu.twoa.dao.TaskDao;
 import by.vsu.twoa.dao.UserDao;
 import by.vsu.twoa.domain.Task;
 import by.vsu.twoa.domain.User;
+import by.vsu.twoa.geometry.Line;
+import by.vsu.twoa.geometry.Point;
+import by.vsu.twoa.geometry.Triangle;
+import by.vsu.twoa.geometry.Vector;
 import by.vsu.twoa.service.exception.ServiceException;
+import by.vsu.twoa.service.exception.TaskNotExistsException;
 import by.vsu.twoa.service.exception.UserNotExistsException;
 
+import java.util.Date;
 import java.util.List;
 
 public class TaskService {
@@ -28,6 +34,38 @@ public class TaskService {
 			List<Task> tasks = taskDao.readByOwner(id);
 			tasks.forEach(task -> task.setOwner(owner));
 			return tasks;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Task findById(Integer id) throws ServiceException {
+		try {
+			Task task = taskDao.read(id).orElseThrow(() -> new TaskNotExistsException(id));
+			task.setOwner(userDao.read(task.getOwner().getId()).orElseThrow(() -> new UserNotExistsException(id)));
+			Triangle triangle = task.getTriangle();
+			Point a = triangle.getVertex1();
+			Point b = triangle.getVertex2();
+			Point c = triangle.getVertex3();
+			Line abSide = new Line(a, b);
+			Line bcSide = new Line(b, c);
+			Line abAltitude = new Line(new Vector(abSide.getA(), abSide.getC()), a);
+			Line bcAltitude = new Line(new Vector(bcSide.getB(), bcSide.getC()), b);
+			task.setAltitudeIntersectionPoint(Line.intersection(abAltitude, bcAltitude).orElse(null));
+			return task;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Integer save(Task task) throws ServiceException {
+		try {
+			if(task.getId() == null) {
+				task.setCreated(new Date(0));
+				return taskDao.create(task);
+			} else {
+				throw new RuntimeException("Update operation not implemented yet");
+			}
 		} catch(DaoException e) {
 			throw new ServiceException(e);
 		}
